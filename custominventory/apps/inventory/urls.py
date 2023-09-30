c7a94d3aa50aaeadca9795ed3d1b00bb027467aa
sergio mondragon <sergio.a.ms.usat@gmail.com>
2023-09-30 00:13:06 -0500

    path of the inventory home

--- custominventory/apps/inventory/urls.py
+++ custominventory/apps/inventory/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .views import InventoryHome
 
 app_name = 'inventory_app'
 
 
 urlpatterns = [
-    
+    path('inventory/',InventoryHome.as_view(),name='inventory_home'),
 ]
