9f1f339e955319d63af1e120eea0cd4ac50be03a
sergio mondragon <sergio.a.ms.usat@gmail.com>
2023-09-29 23:58:45 -0500

    Inventory home view

--- custominventory/apps/inventory/views.py
+++ custominventory/apps/inventory/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+class InventoryHome(TemplateView):
+    template_name = "inventory/inventory_home.html"
