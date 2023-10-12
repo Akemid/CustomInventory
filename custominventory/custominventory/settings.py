1606c472d20be25640366886005c45affdd579e0
akemid <sergiomondragondev@gmail.com>
2023-10-11 21:24:20 -0500

    Just allowed host configuration to all

--- custominventory/custominventory/settings.py
+++ custominventory/custominventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9uz0_&vd(1$%n1(kxe--7_bhkrcviw17lk35on(j9d53%8aqdp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
